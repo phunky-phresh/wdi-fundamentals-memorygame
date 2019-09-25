e921004ad8485c4218924f044868e8027cc0342d
ncooney94 <NickCooney@192-168-1-9.tpgi.com.au>
2019-09-25 16:20:50 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,20 +5,39 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = []
 
-var cardOne = cards[0];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]); 
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!");
+	else {
+		alert("Sorry, try again.");
+	}
+	{
+		checkForMatch();
+	}
+}
+//console.log("User flipped " + cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+
+/*var cardOne = cards[0];
 var cardTwo = cards[2];
 
 cardsInPlay.push(cardOne);
 cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert('Sorry, try again');
-	}
-
-}
+console.log("User flipped " + cardTwo); */
